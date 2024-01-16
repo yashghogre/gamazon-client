55f3f417a6c74338dd5dd84ed95772e9e82709cb
Yash <yashghogre100@gmail.com>
2024-01-16 22:54:37 +0530

    Initial Commit

---
+++ src/app/Upload-Game/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '@/styles/upload-game.module.css'
+
+const page = () => {
+  return (
+    <div className={styles.mainDiv}>
+        <div className={styles.titleDiv}>
+            <h1>Upload your Game</h1>
+        </div>
+    </div>
+  )
+}
+
+export default page
