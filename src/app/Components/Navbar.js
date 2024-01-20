21c5a79a68b3b8898c584ffff4eb58f21d26c796
Yash <yashghogre100@gmail.com>
2024-01-20 22:27:33 +0530

    Made the game page

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -2,28 +2,31 @@ import React from 'react'
 import styles from '@/styles/Navbar.module.css'
 import Image from 'next/image'
 import { FaSearch } from "react-icons/fa";
+import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-    <div className={styles.mainDiv}>
-        <div className={styles.logoDiv}>
-            <Image src={'/logo.png'} height={100} width={100} alt='logo' className={styles.logo} />
+    return (
+        <div className={styles.mainDiv}>
+            <div className={styles.logoDiv}>
+                <Link href={'/'}>
+                    <Image src={'/logo.png'} height={100} width={100} alt='logo' className={styles.logo} />
+                </Link>
+            </div>
+            <div className={styles.searchDiv}>
+                <form className={styles.form}>
+                    <input placeholder='Search for your game...' className={styles.input} />
+                    {/* <input type='submit' name='search' className={styles.searchBtn} /> */}
+                    <button type='submit' name='search' className={styles.searchBtn}><FaSearch /></button>
+                    {/* <button type='submit' name='search' classname={styles.searchBtn}>Search</button> */}
+                </form>
+            </div>
+            <div className={styles.menuDiv}>
+                <p>Genre</p>
+                <p>Login</p>
+                <p>SignUp</p>
+            </div>
         </div>
-        <div className={styles.searchDiv}>
-            <form className={styles.form}>
-                <input placeholder='Search for your game...' className={styles.input} />
-                {/* <input type='submit' name='search' className={styles.searchBtn} /> */}
-                <button type='submit' name='search' className={styles.searchBtn}><FaSearch /></button>
-                {/* <button type='submit' name='search' classname={styles.searchBtn}>Search</button> */}
-            </form>
-        </div>
-        <div className={styles.menuDiv}>
-            <p>Genre</p>
-            <p>Login</p>
-            <p>SignUp</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
--- src/app/game/[gameID]/page.js
+++ src/app/game/[gameID]/page.js
@@ -1,11 +1,86 @@
 'use client'
 
-const gameID = ({params}) => {
-  return (
-    <div>
-        <h4>The game ID is {params.gameID}</h4>
-    </div>
-  )
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react"
+import styles from '@/styles/GamePage.module.css'
+
+const gameID = ({ params }) => {
+
+    const gID = params.gameID;
+    const [rd, setRD] = useState([])
+
+    console.log(gID)
+
+    useEffect(() => {
+        async function fetchGame() {
+            const response = await fetch('http://localhost:8080/api/game/checkGame', {
+                method: 'post',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ gameID: gID })
+            });
+            // console.log(response)
+            const resData = await response.json();
+            // console.log(resData);
+            // setRD((prev) => [...prev, resData])
+            setRD([resData])
+            // setRD(resData)
+        };
+
+        fetchGame();
+    }, [])
+
+    console.log(rd)
+    // console.log(rd.gameExist)
+
+    return (
+        <div style={{marginTop: '5vh'}}>
+            {/* <h4>{rd.name}</h4>
+            <p>{rd.description}</p> */}
+            {rd.map((value, key) => {
+                return (
+                    <div>
+                        <div className={styles.mainDiv}>
+                            <div>
+                                <Image src={'/k1.jpeg'} height={500} width={500} alt="game-image" className={styles.img} />
+                            </div>
+                            <div>
+                                <div className={styles.nameDiv}>
+                                    <h1>{value.name}</h1>
+                                </div>
+                                <div className={styles.infoDiv}>
+                                    <div className={styles.sIDiv} style={{ width: '20vw' }}>
+                                        <p className={styles.title}>Price</p>
+                                        <p className={styles.title}>Uploader</p>
+                                        <p className={styles.title}>Genre</p>
+                                        <p className={styles.title}>Required RAM</p>
+                                        <p className={styles.title}>Required GPU</p>
+                                        <p className={styles.title}>description</p>
+                                    </div>
+                                    <div className={styles.sIDiv} style={{ width: '30vw' }}>
+                                        <p>₹ {value.price}</p>
+                                        <p>{value.uploader}</p>
+                                        <p>{value.genre}</p>
+                                        <p>{value.ram}</p>
+                                        <p>{value.gpu}</p>
+                                        <p>{value.description}</p>
+                                    </div>
+                                </div>
+                                {/* <p>{value.comments}</p> */}
+                            </div>
+                        </div>
+                        <div className={styles.btnDiv}>
+                            <Link href={`${value.link}`}>
+                                <button className={styles.btn}>Download</button>
+                            </Link>
+                        </div>
+                    </div>)
+            })}
+        </div>
+    )
 }
 
 export default gameID
