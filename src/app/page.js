d52a4459a9f3274c4bc2008ebd20c2053f9e0a29
Yash <yashghogre100@gmail.com>
2024-02-15 22:38:52 +0530

    Added data in db and links in client

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styles from './page.module.css'
 import SOTCard from './Components/SOTCard'
 import HomeCarousel from './Components/HomeCarousel'
 import GameCard from './Components/GameCard'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -23,13 +24,21 @@ export default function Home() {
       </div>
       <div>
         <div className={styles.DAGTitleDiv}>
-          <h3>Discover <span style={{backgroundColor: 'red', paddingLeft: '2px', paddingRight: '2px', borderRadius: '2px'}}>Action</span> Games</h3>
+          <h3>Discover <span style={{ backgroundColor: 'red', paddingLeft: '2px', paddingRight: '2px', borderRadius: '2px' }}>Action</span> Games</h3>
         </div>
         <div className={styles.DAGCards}>
-          <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
-          <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
-          <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
-          <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
+          <Link href={'http://localhost:3000/game/65ce434450623f8deb4dabec'} style={{ textDecoration: 'none' }}>
+            <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
+          </Link>
+          <Link href={'http://localhost:3000/game/65ce434450623f8deb4dabec'} style={{ textDecoration: 'none' }}>
+            <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
+          </Link>
+          <Link href={'http://localhost:3000/game/65ce434450623f8deb4dabec'} style={{ textDecoration: 'none' }}>
+            <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
+          </Link>
+          <Link href={'http://localhost:3000/game/65ce434450623f8deb4dabec'} style={{ textDecoration: 'none' }}>
+            <GameCard img='/cs2.jpg' name='Counter Strike 2' offerTill='Offer ends on 31st' offer='-69%' price='555' />
+          </Link>
         </div>
       </div>
     </main>
