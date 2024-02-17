77da668f2514ff34259a4acb1ef48f695c302a98
Yash <yashghogre100@gmail.com>
2024-02-17 19:16:00 +0530

    Connected client to server for auth

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,25 +1,77 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from '@/styles/Signup.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+// import toast, { Toaster } from 'react-hot-toast'
 
 const page = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    const router = useRouter()
+
+    const userData = (e) => {
+        const { name, value } = e.target
+
+        setUser((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    console.log(user)
+
+    const sendData = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch('http://localhost:8080/api/auth/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+
+        const resData = res.json()
+        console.log(res.status)
+
+        // setUser({
+        //     email: '',
+        //     password: ''
+        // }
+        // )
+
+        if (res.status === 200) {
+            // toast.success('User Logged in Successfully!')
+            router.push('/')
+        }
+    }
+
     return (
         <div>
             <div className={styles.mDiv}>
+                {/* <Toaster /> */}
                 {/* <h3 className={styles.loginTitle}>Login</h3> */}
                 <form className={styles.form}>
                     <fieldset className={styles.fs}>
                         <legend className={styles.lg}><h2>Login</h2></legend>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>email</label>
-                            <input type='text' name='email' className={styles.input} />
+                            <input type='email' name='email' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>password</label>
-                            <input type='text' name='password' className={styles.input} />
+                            <input type='password' name='password' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.btnDiv}>
-                            <button className={styles.btn}>Sign-Up</button>
+                            <button className={styles.btn} onClick={sendData}>Login</button>
                         </div>
                         <div style={{ display: 'flex', justifyContent: 'center' }}>
                             <Link href='/signup' style={{ textDecoration: 'none' }}><p style={{ color: '#4169E1', outline: '1px' }}>New User?</p></Link>
@@ -27,6 +79,7 @@ const page = () => {
                     </fieldset>
                 </form>
             </div >
+            {/* <Toaster /> */}
         </div >
     )
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,10 +1,58 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import styles from '@/styles/Signup.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+// import toast, { Toaster } from 'react-hot-toast'
 
 const page = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        phone: '',
+        email: '',
+        password: ''
+    })
+
+    const router = useRouter()
+
+    const userData = (e) => {
+        const { value, name } = e.target
+
+        setUser((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+    // console.log(user)
+
+    const sendData = async (e) => {
+        e.preventDefault()
+
+        const res = await fetch('http://localhost:8080/api/auth/register', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+
+        const resData = res.json()
+        console.log(res.status)
+
+        if (res.status === 201) {
+            // toast.success('User Successfully Registered!')
+            router.push('/login')
+        }
+    }
+
     return (
         <div>
+            {/* <Toaster /> */}
             <div className={styles.mDiv}>
                 {/* <h3 className={styles.title}>Sign-up</h3> */}
                 <form className={styles.form}>
@@ -12,22 +60,22 @@ const page = () => {
                         <legend className={styles.lg}><h2>Sign-Up</h2></legend>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>Username</label>
-                            <input type='text' name='username' className={styles.input} />
+                            <input type='text' name='username' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>phone</label>
-                            <input type='text' name='phone' className={styles.input} />
+                            <input type='number' name='phone' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>email</label>
-                            <input type='text' name='email' className={styles.input} />
+                            <input type='email' name='email' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.inDiv}>
                             <label className={styles.label}>password</label>
-                            <input type='text' name='password' className={styles.input} />
+                            <input type='password' name='password' className={styles.input} onChange={userData} />
                         </div>
                         <div className={styles.btnDiv}>
-                            <button className={styles.btn}>Sign-Up</button>
+                            <button className={styles.btn} onClick={sendData}>Sign-Up</button>
                         </div>
                         <div style={{ display: 'flex', justifyContent: 'center' }}>
                             <Link href='/login' style={{ textDecoration: 'none' }}><p style={{ color: '#4169E1' }}>Already a user?</p></Link>
